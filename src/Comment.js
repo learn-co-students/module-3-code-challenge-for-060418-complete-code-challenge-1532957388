ce83a899a57928b791b4021c41952c966d300fd2
jarretbryan <jarret.bryan@gmail.com>
2018-07-30 11:00:29 -0400

    added comments re commment sorting

--- src/Comment.js
+++ src/Comment.js
@@ -11,6 +11,8 @@ function renderComments(commentArr){
     } ).join("")
     commentBox.innerHTML = commentHTML
 }
+// this sorts comments by ID, but I can't tell how IDs are assigned - if I wanted them in order of creation, I would sort by the 'created_at' attribute, and then map over *that* array to create the commentHTML. ran out of time on this one.
+
 
 function optCommentRender(unSavedComment) {
     commentBox.innerHTML += `<li>${unSavedComment}</li><br>`
